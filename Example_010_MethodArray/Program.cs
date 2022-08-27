64cac14d94a2d0ea70c16235fc29e135d8c92821
k <k@m15.dzm>
2022-08-27 10:22:43 +0300

    SEM_2_Ex_1

---
+++ Example_010_MethodArray/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine("Введите первое число: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+int maxnumber = 0;
+
+if (number1 > number2)
+{
+    maxnumber = number1;
+    Console.Write("Большее число = " + maxnumber);
+}
+
+else if (number1 == number2)
+{
+    Console.Write("Числа равны ");
+}
+else 
+{
+    maxnumber = number2;
+    Console.Write("Большее число =" + maxnumber);
+}
