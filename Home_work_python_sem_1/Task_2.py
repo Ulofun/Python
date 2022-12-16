134556556d7beae7456b2e7e472534e8e5d2e589
Ulofun <rakovod1337@gmail.com>
2022-12-16 18:24:16 +0300

    HomeWork sem_1

---
+++ Home_work_python_sem_1/Task_2.py
@@ -0,0 +1,18 @@
+# 2. Напишите программу для проверки ложности утверждения
+# (W ⋀ Z) ⋁ ¬Y ⋁ (¬X ≡ ¬W) для всех значений предикат.
+
+
+lst = [0, 1]
+flag = True
+for x in lst:
+    for y in lst:
+        for z in lst:
+            f1 = not (x or y or z)
+            f2 = not (x) and not (y) and not (z)
+            print(x, y, z, f1 == f2)
+            if f1 != f2:
+                flag = False
+if flag:
+    print('истинно')
+else:
+    print('ложно')
